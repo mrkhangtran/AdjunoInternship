71aae0e0564a346ddcf26ffd9b127f5abe0f5acc
duynguyenkos <nkduyhcmup@gmail.com>
2018-12-05 17:52:13 +0700

    add_progresschecktool

---
+++ BLL_Layer/BLL/Implements/ProgressCheckTool/ProgressCheckRepository.cs
@@ -0,0 +1,38 @@
+﻿using BLL_Layer.BLL.Interface.ProgressCheckTool;
+using DAL_Layer.DAL.Models.OrderTrack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL_Layer.BLL.Implements.ProgressCheckTool
+{
+    class ProgressCheckRepository : IProgressCheckRepository
+    {
+        public void Add(ProgressCheckModel progressCheckModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(ProgressCheckModel progressCheckModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ProgressCheckModel Edit(ProgressCheckModel progressCheckModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ProgressCheckModel Find(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ProgressCheckModel> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
